4fdef1f8fc693a75b1331a51517d1bfd15f78e35
bruno <modestobruno79@gmail.com>
2025-05-09 07:27:53 -0300

    first commit

---
+++ app/models/Pessoa.java
@@ -0,0 +1,15 @@
+package models;
+
+import play.mvc.Controller;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+
+public class Pessoa extends Model {
+
+	public String nome;
+	public String email;
+}
